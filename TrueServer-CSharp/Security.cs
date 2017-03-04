9f1078bd6403116d4b4aae043adaf98527036f62
DESKTOP-7UATI9L\Izzy <sfrank@true-server.de>
2017-03-04 17:22:50 +0100

    Added Class(es) AES.CS, Security.CS

---
+++ TrueServer-CSharp/Security.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrueServer_CSharp
+{
+    class Security
+    {
+        public String Encrypt(String inputStr, String salt)
+        {
+            //"";
+            AES aes_obj = new AES();
+            byte[] passwordBytes = aes_obj.GetPasswordBytes(salt);
+            String OutText = AES.Encrypt(inputStr, passwordBytes);
+
+            String returnStr = OutText;
+            return returnStr;
+        }
+
+        public String Decrypt(String inputStr, String salt)
+        {
+            AES aes_obj = new AES();
+            byte[] passwordBytes = aes_obj.GetPasswordBytes(salt);
+            //Console.WriteLine(OutText);
+            String OutText = AES.Decrypt(inputStr, passwordBytes);
+            String returnStr = OutText;
+            return returnStr;
+        }
+    }
+}
